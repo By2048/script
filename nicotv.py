e55d8cf03cdde0feb23f0113d9f986b5dd32aaf2
By2048 <1247079575@qq.com>
2021-02-12 23:11:20 +0800

    add chrome selenium

--- nicotv.py
+++ nicotv.py
@@ -1,5 +1,7 @@
 import os
 import html
+import time
+import copy
 import base64
 from pprint import pprint
 from collections import namedtuple
@@ -7,9 +9,28 @@ from typing import List, Union
 
 import requests
 from bs4 import BeautifulSoup
+from rich import print
 from rich.prompt import Prompt
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 base_url = "http://www.nicotv.club"
+chrome_driver_path = r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+
+screen_x, screen_y = 0, 0
+screen_xx, screen_yy = 3840, 2160
+screen_w = screen_xx - screen_x
+screen_h = screen_yy - screen_y
+screen_dpi = 2
+
+w = screen_w * (5 / 6)
+h = screen_h * (8 / 9)
+x = screen_x + (screen_w - w) / 2
+y = screen_y + (screen_h - h) / 2
+
+x, y = x / screen_dpi, y / screen_dpi
+w, h = w / screen_dpi, h / screen_dpi
+x, y, w, h = int(x), int(y), int(w), int(h)
 
 
 class Video(object):
@@ -58,18 +79,34 @@ def get_video(play_url) -> Video:
     download_url = base64.b64decode(download_url).decode()
     download_url = download_url.split("?url=")[-1]
     download_url = download_url.rstrip("?")
-    video.download_url = download_url
 
+    if ".mp4" not in download_url:
+        chrome_options = Options()
+        chrome_options.add_argument(f'--window-size={w},{h}')
+        chrome_options.add_argument(f'--window-position={x},{y}')
+
+        chrome = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=chrome_options)
+
+        chrome.get(play_url)
+        chrome.implicitly_wait(10)
+
+        chrome.switch_to.frame(0)
+        chrome.switch_to.frame(0)
+
+        element_video = chrome.find_element_by_tag_name("video")
+        download_url = element_video.get_attribute("src")
+        chrome.close()
+
+    video.download_url = download_url
     return video
 
 
 def download_video(video: Video):
-    video_type = video.download_url.split(".")[-1]
-    cmd = f"D:\\Aria2\\aria2c.exe " \
-          f"{video.download_url} " \
-          f"--out '{video.title}.{video_type}'"
+    cmd = f" D:\\Aria2\\aria2c.exe " \
+          f" '{video.download_url}' " \
+          f" --out '{video.title}.mp4' "
     cmd = cmd.replace("'", '"')
-    print(cmd)
+    print(f"[red]{copy.copy(cmd)}[/red]")
     os.system(cmd)
 
 
@@ -88,15 +125,21 @@ def main():
 
 
 def test():
-    detail_url = "http://www.nicotv.me/video/detail/58086.html"
-    play_url = "http://www.nicotv.me/video/play/58086-1-5.html"
+    # chrome
+    detail_url = "http://www.nicotv.me/video/detail/56828.html"
+    play_url = "http://www.nicotv.me/video/play/56828-1-1.html"
+
+    # requests
+    # detail_url = "http://www.nicotv.me/video/detail/58100.html"
+    # play_url = "http://www.nicotv.club/video/play/58100-1-1.html"
 
     # links = get_all_link(detail_url)
     # for link in links:
     #     print(link)
 
-    video = get_video(play_url)
-    print(video)
+    # video = get_video(play_url)
+    # print(video)
+    # download_video(video)
 
 
 if __name__ == '__main__':
