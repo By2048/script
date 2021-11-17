4393436afe21d72813439fe4aeebc88e97d3d562
By2048 <1247079575@qq.com>
2021-11-17 22:27:51 +0800

    update pyenv use fire

--- other/pyenv.py
+++ other/pyenv.py
@@ -1,70 +1,87 @@
 import os
+import re
 import sys
 import time
+from pathlib import Path
 
 import fire
+from rich import print
+from rich.pretty import pprint
 
 folder = "D:\\Python\\"
+jx = "https://pypi.tuna.tsinghua.edu.cn/simple"
+requirements = Path("E:\\Project\\_pip_\\requirements.txt")
+
+pythons = []
+for item in Path(folder).iterdir():
+    if re.match(r"^(\d)(\.)(\d+)(.)(\d+)$", item.name):
+        pythons.append(item.name)
 
 
 def show():
-    data = os.listdir(folder)
-    data = [item for item in data if not (item.startswith("_") and item.endswith("_"))]
-    print(data)
+    for item in Path(folder).iterdir():
+        if item.name.endswith(".ini"):
+            continue
+        print(item.name)
 
 
-"""
+def create(source, target):
+    """ 创建环境
+    @param source: 原始环境
+    @param target: 目标环境
+    @return:
+    """
 
-D:\python\3.8.9\python.exe -m pip install --upgrade pip
+    path_target = Path(f"{folder}\\{target}")
 
+    if source not in pythons:
+        print("[red]ERROR : source not in pythons")
+        print(f"[green] source = {source}")
+        print(f"[green]pythons = {pythons}")
+        return
 
-D:\Python\3.8.9\Scripts\pip.exe install virtualenv
+    if re.match(r"^(\d)(\.)(\d+)(.)(\d+)$", target):
+        print(f"[red]ERROR : target name = {target}")
+        return
 
+    if not path_target.exists():
+        path_target.mkdir(parents=True, exist_ok=True)
 
-D:\Python\3.8.9\Scripts\virtualenv.exe D:\Python\_3.8.9_\ --python=D:\python\3.8.9\python.exe
-
-
-D:\Python\_3.8.9_\Scripts\activate.bat
-
-
-
-
-"""
-
-
-def create(source, name):
     command = fr"""
-    {folder}\{source}\python.exe -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
-    {folder}\{source}\Scripts\pip.exe install virtualenv -i https://pypi.tuna.tsinghua.edu.cn/simple
-    {folder}\{source}\Scripts\virtualenv.exe {folder}\{name}\ --python={folder}\{source}\python.exe
+
+    {folder}\{source}\python.exe -m pip install --upgrade pip -i {jx}
+    {folder}\{source}\Scripts\pip.exe install virtualenv --upgrade -i {jx}、
+    {folder}\{source}\Scripts\virtualenv.exe {folder}\{target}\ --python={folder}\{source}\python.exe
     
+    {folder}\{target}\Scripts\pip.exe install -r {requirements} -i {jx}
+
     """
     command = command.replace("\\\\", "\\")
     command = command.strip()
 
     for cmd in command.split("\n"):
         cmd = cmd.strip()
+        if not cmd:
+            continue
         print(cmd)
         os.system(cmd)
-        time.sleep(3)
+        print()
+        print()
+        print()
 
 
-def activate(name):
-    command = fr"{folder}\{name}\Scripts\activate.bat"
+def activate(target):
+    command = fr"{folder}\{target}\Scripts\activate.ps1"
     command = command.replace("\\\\", "\\")
     command = command.strip()
     os.system(command)
 
 
-def delete(env_name):
-    pass
+def delete(target):
+    path_target = Path(f"{folder}\\{target}")
+    path_target.rmdir()
+    print(f"delete {path_target}")
 
 
 if __name__ == '__main__':
-    pass
-    # fire.Fire()
-    # list()
-    # pass
-    # show()
-    # create("3.7.8", "_python_")
-    # create("3.9.4", "_3.9.4_")
+    fire.Fire()
