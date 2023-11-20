d9c4fba0889386a8522faf3a9e9f91ce2350bc3b
By2048 <1247079575@qq.com>
2023-11-21 02:07:40 +0800

    初步实现配置文件格式化功能

--- tool/vscode.py
+++ tool/vscode.py
@@ -2,75 +2,124 @@ import os
 import re
 import copy
 import json
+import shutil
 from pprint import pprint
+from pathlib import WindowsPath
 
-path = r"D:\VSCodeData\User\keybindings.json"
-path_format = r"E:\Project\Demo\json.json"
+# https://code.visualstudio.com/docs/getstarted/keybindings
+
+path_source = WindowsPath(r"D:\VSCodeUser\User\KeyBindings.json")
+path_backup = WindowsPath(r"D:\VSCodeUser\User\KeyBindings.Backup.json")
+path_format = WindowsPath(r"D:\VSCodeUser\User\KeyBindings.Format.json")
+
+shutil.copy(path_source, path_backup)
+
+print()
+print(f"   {path_source} \n"
+      f"-> {path_backup}")
+print()
+
+print(f"path_source {path_source}")
+print(f"path_backup {path_backup}")
+print(f"path_format {path_format}")
+print()
 
 
-def json_print(data: dict):
-    key = data.get("key")
-    command = data.get("command")
-    when = data.get("when")
+def json_format(data: dict):
+    key = data.get("key", "")
+    command = data.get("command", "")
+    when = data.get("when", "")
 
     key = key.replace("\\", "\\\\")
     command = command.replace("\\", "\\\\")
-    when = when.replace("\\", "\\\\") if when else ...
+    when = when.replace("\\", "\\\\")
 
-    result = f"""{{  "key"     : "{key}" ,\n   "command" : "{command}" """
-    if when:
-        result += ",\n"
-        result += f"""   "when"    : "{when}"  }}, """
+    result = ""
+    result += f'{{ "key" : "{key}" {" " * (30 - len(key))}  ,  '
+    result += f'"command" : "{command}" {" " * (60 - len(command))}'
+    if not when:
+        result += " }  ,"
     else:
-        result += " },"
+        result += f'    ,  "when" : "{when}" }}, '
+
     return result
 
 
-def main():
-    data = ""
+def format_config():
+    with open(path_backup, 'r', encoding='utf-8') as file:
+        data = file.readlines()
 
-    with open(path, 'r', encoding='utf-8') as file:
-        config = file.readlines()
+    data = "".join([item.lstrip() for item in data])
 
-    for item in config:
-        if not item:
-            continue
-        item = item.lstrip()
-        data += item
+    re_key = '"[\\\d\w\+\s_]+"'
+    re_command = '"[\\\d\w\-\+\.,]+"'
+    re_when = '"[\s\S]+?"'
 
-    data = re.sub(r"{\n", "{", data)
-    data = re.sub(r",\n", ",", data)
-    data = re.sub(r"\n},", "},\n", data)
-    data = re.sub(r"\n}", "},\n", data)
+    _match = rf'''
+    1      2        3 key        4    5         6 command
+    (\{{\n)("key": )({re_key})(,\n)("command": )({re_command})(,?)(\n\}})(,?)
+    '''.strip().split("\n")[-1].strip()
+    print(_match)
+    _get = r'{ "key" : \3 , "command" : \6 } ,'
+    data = re.sub(_match, _get, data)
 
-    data = data.split("\n")
+    _match = rf'''
+    1     2        3 key        4    5          6 command    7    8         9 when
+    (\{{\n)("key": )({re_key})(,\n)("command": )({re_command})(,\n)("when": )({re_when})(\n\}})(,?)
+    '''.strip().split("\n")[-1].strip()
+    print(_match)
+    _get = r'{ "key" : \3 , "command" : \6 , "when" : \9 } ,'
+    data = re.sub(_match, _get, data)
+
+    _match = rf'''
+    1     2      3      4        5 key     6    7      8            9 command
+    (//\s)(\{{\n)(//\s+)("key": )({re_key})(,\n)(//\s+)("command": )({re_command})(,?)(\n)(//\s+\}})(,?)
+    '''.strip().split("\n")[-1].strip()
+    print(_match)
+    _get = r'// { "key" : \5 , "command" : \9 } ,'
+    data = re.sub(_match, _get, data)
+
+    _match = rf'''
+    1     2      3      4        5 key     6    7      8            9 command     10   11     12        13 when
+    (//\s)(\{{\n)(//\s+)("key": )({re_key})(,\n)(//\s+)("command": )({re_command})(,\n)(//\s+)("when": )({re_when})(\n//\s\}})(,?)
+    '''.strip().split("\n")[-1].strip()
+    print(_match)
+    _get = r'// { "key" : \5 , "command" : \9 , "when" : \13 } ,'
+    data = re.sub(_match, _get, data)
+
+    with open(path_format, "w", encoding='utf-8') as file:
+        file.write(data)
+
+
+def test():
+    with open(path_format, 'r', encoding='utf-8') as file:
+        data = file.readlines()
+    data = [item.strip() for item in data]
 
     content = ""
     for index, item in enumerate(data):
         if item.startswith("//"):
-            content += item
-            content += "\n"
+            # if item == "// ":
+            #     content += "\n"
+            # else:
+            #     content += item
+            content += item + "\n"
             continue
         if item.endswith(","):
             item = item.rstrip(",")
         try:
             item = json.loads(item)
-            item = json_print(item)
+            item = json_format(item)
             content += item
         except json.decoder.JSONDecodeError:
             content += item
         content += "\n"
 
-    with open(path_format, "w") as file:
+    path_test = WindowsPath(r"T:\KeyBindings.Test.json")
+    with open(path_test, "w", encoding='utf-8') as file:
         file.write(content)
 
 
-def test():
-    pass
-
-
 if __name__ == '__main__':
-    print("\n" * 3)
-    main()
-    print("\n" * 3)
+    format_config()
     # test()
