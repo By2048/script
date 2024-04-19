2be918465bedd05f84d5fd809b449f0347d15a4b
By2048 <1247079575@QQ.Com>
2024-04-20 05:59:54 +0800

    优化B站视频解析工具

--- bilibili/download_video_join.py
+++ bilibili/download_video_join.py
@@ -1,34 +1,34 @@
 import os
 import sys
 import json
-import time
 import shutil
-import subprocess
-import logging
 from datetime import datetime
 
+from pathlib import WindowsPath
+from typing import List
+
+from rich import print
+from rich.prompt import Prompt
+
 try:
-    from tool.rename import change_name
+    from rename.rename import change_name
 except ImportError:
     path = os.path.dirname(os.path.dirname(__file__))
     sys.path.append(path)
-    from tool.rename import change_name
+    from rename.rename import change_name
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(levelname)1.1s %(lineno)3d] %(message)s',
-    datefmt='%H:%M:%S'
-)
+path_ffmpeg = WindowsPath("D:\\FFmpeg\\ffmpeg.exe")
+path_download = WindowsPath("T:\\#Sync\\Video\\")
+path_video = path_download
 
-base_folder = "T:\\com.bilibili.app.in\\"
-ffmpeg_path = "D:\\FFmpeg\\#\\bin\\ffmpeg.exe"
-download_path = "T:\\com.bilibili.app.in\\"
-keep_path = "T:\\"
+if os.environ.get("Debug"):
+    path_download = WindowsPath("V:\\Video\\")
+    path_video = WindowsPath("V:\\")
 
-"""
-bilibili (download_path)
-├─xxxxxxxx (Collection)
-│  ├─1 (video1)
+r"""
+V:\Video\ (Path Download)
+├─1010101 (Collection)
+│  ├─c_101 (Video)
 |  |  |-80
 |  |  |  audio.m4s
 │  |  |  index.json
@@ -47,12 +47,18 @@ bilibili (download_path)
 
 
 class Video(object):
-    def __init__(self, folder="", index=0, part="", title=""):
-        self.folder = folder
-        self.index = index
-        self.part = part
-        self.title = title
-        self.files = []
+    def __init__(self):
+        self.owner = ""
+        self.folder: WindowsPath = WindowsPath()
+        self.index = ""
+        self.part = ""
+        self.title = ""
+        self.entry: WindowsPath = WindowsPath()
+        self.video: WindowsPath = WindowsPath()
+        self.audio: WindowsPath = WindowsPath()
+
+    def __bool__(self):
+        return bool(self.entry) and bool(self.video) and bool(self.audio)
 
     def __str__(self):
         result = f"{'-> video_index'.rjust(20)} {self.index}\n"
@@ -63,7 +69,7 @@ class Video(object):
 
 class Collection(object):
     def __init__(self, folder):
-        self.folder: str = folder
+        self.folder: WindowsPath = folder
         self.videos: [Video] = []
 
     @property
@@ -71,129 +77,111 @@ class Collection(object):
         return len(os.listdir(self.folder))
 
     def __str__(self):
-        result = f"{'collection_path'.rjust(20)} {self.folder}"
+        result = f"  collection  {self.folder}"
         return result
 
 
-def logging_config():
-    logging.info('')
-    logging.info('-' * 66)
-    logging.info(f'ffmpeg_path      {ffmpeg_path}')
-    logging.info(f'download_path    {download_path}')
-    logging.info(f'keep_path        {keep_path}')
-    logging.info('-' * 66)
+def print_config():
+    print()
+    print(f'  [green]FFmpeg[/green]    {path_ffmpeg}')
+    print(f'  [green]Download[/green]  {path_download}')
+    print(f'  [green]Video[/green]     {path_video}')
 
 
 def init_collections():
     collections = []
 
-    for collection_folder in os.listdir(download_path):
-        collection_folder_path = os.path.join(download_path, collection_folder)
+    for collection_folder in path_download.iterdir():
+        collection_folder_path = path_download / collection_folder
+        if not collection_folder_path.is_dir():
+            continue
         collection = Collection(folder=collection_folder_path)
         videos = []
-        for video_folder in os.listdir(collection_folder_path):
-            video_folder_path = os.path.join(collection_folder_path, video_folder)
-            video_folder = video_folder.replace("c_", "")
-            video = Video(folder=video_folder_path, index=int(video_folder))
+        for video_folder in collection_folder_path.iterdir():
+            video = Video()
+            video.folder = collection_folder_path / video_folder
+            video.index = video_folder.name.replace("c_", "")
             videos.append(video)
         collection.videos = videos
-        logging.info(collection)
         collections.append(collection)
-
-    logging.info('-' * 66)
-    logging.info('')
-    logging.info('')
 
     return collections
 
 
 def init_collection_videos(collection: Collection):
+    video: Video
     for video in collection.videos:
-        json_file_path = os.path.join(video.folder, 'entry.json')  # 视频信息
 
         # 删除较小分辨率的视频
-        video_resolution_items = os.listdir(video.folder)
-        video_resolution_items.remove('danmaku.xml')
-        video_resolution_items.remove('entry.json')
-        if len(video_resolution_items) > 1:
-            _item_ = sorted(video_resolution_items, key=lambda x: int(x))
-            video_resolution_min = os.path.join(video.folder, _item_[0])
-            shutil.rmtree(video_resolution_min)
+        video_items: List[WindowsPath] = list(video.folder.iterdir())
+        video_items = [item for item in video_items if item.is_dir()]
+        if len(video_items) > 1:
+            video_items = sorted(video_items, key=lambda x: int(x.name), reverse=True)
+        video_item = video_items[0]
+
+        video.entry = video.folder / 'entry.json'
 
         # 初始化视频文件
-        video_file_folder = video_resolution_items[-1]
-        video_file_folder = os.path.join(video.folder, video_file_folder)
-        for video_file in os.listdir(video_file_folder):
-            if video_file.endswith('.json'):
-                continue
-            video.files.append(os.path.join(video_file_folder, video_file))
+        video.folder = video_item
+        video.video = video_item / 'video.m4s'
+        video.audio = video_item / 'audio.m4s'
 
-        with open(json_file_path, 'r', encoding='utf-8') as file:
+        with open(video.entry.as_posix(), 'r', encoding='utf-8') as file:
             data = json.load(file)
+            owner = data.get('owner_name')
             part = data['page_data']['part']
             title = data['title']
             part = change_name(part)
             title = change_name(title)
+            video.owner = owner
             video.part = part
             video.title = title
-
-    # if data.get('ep', ''):
-    #     index = data['ep']['index']
-    #     index_title = data['ep']['index_title']
-    #     video_name = data['title'] + '_' + index
-    #     if index_title:
-    #         video_name = data['title'] + '_' + index + '_' + index_title
-    #     video_name = change_name(video_name)
-    #     return video_name
 
 
 def join_video(video: Video):
     # 使用 ffmpeg 拼接视频
 
-    if len(video.files) < 2:
-        logging.error('')
-        logging.error('')
-        logging.info('-' * 66)
-        logging.error(f"下载文件缺失")
-        logging.error(f"\t{video.title}")
-        logging.error(f"\t{video.folder}")
-        logging.error(f"\t{str(video.files)}")
-        logging.info('-' * 66)
-        logging.error('')
-        logging.error('')
+    if not video:
+        print('')
+        print('')
+        print('-' * 66)
+        print(f"下载文件缺失")
+        print(f"  {video.title}")
+        print(f"  {video.folder}")
+        print('-' * 66)
+        print('')
+        print('')
         return
 
-    logging.info('-' * 66)
+    name_config = (
+        f"{video.owner} {video.title}",
+        f"{video.title}",
+        f"{video.part}" if video.title != video.part else "",
+        f"{video.title} {video.part}" if video.title != video.part else "",
+        f"{video.index} {video.title}",
+        f"{video.index} {video.part} {video.title}" if video.part != video.title else "",
+        f"{datetime.now().strftime('%Y-%m-%d')} {video.title}",
+    )
+    name_config = (item for item in name_config if item)
+    names = {}
+    for index, name in enumerate(name_config, 1):
+        if name:
+            names[f"{index}"] = name
 
-    video_m4s = video.files[0]
-    audio_m4s = video.files[1]
-    if "video" not in video_m4s or "audio" not in audio_m4s:
-        video_m4s, audio_m4s = audio_m4s, video_m4s
+    print()
+    data = f"  # | [reverse]{video.folder}[/reverse] \n"
+    for index, value in names.items():
+        data += f"  [red]{index}[/red] | {value}\n"
+    data = data.rstrip()
 
-    names = {
-        "1": f"{video.title}",
-        "2": f"{video.part}",
-        "3": f"{video.title} {video.part}",
-        "4": f"{video.index} {video.title} {video.part}",
-        "5": f"{datetime.now().strftime('%Y-%m-%d')} {video.title}",
-        "0": "[X]"
-    }
+    print(data)
 
-    data = "#选项# \n"
-    for key, value in names.items():
-        data += f"\t\t[#{key}] {value}\n"
-    data = data.rstrip("\n")
-    logging.info(data)
-
-    check = None
-    while True:
-        check = input(" #选择# ".rjust(13))
-        if check:
-            break
+    check = Prompt.ask(f"  [red]#选择#[/red]", default="\\")
 
     check = "1" if check == "\\" else check
-    if check == "0":
+    if check == "\\\\":
         return
+
     if check not in names.keys():
         return
 
@@ -201,24 +189,30 @@ def join_video(video: Video):
     if not name:
         return
 
-    video_output = os.path.join(keep_path, name)
+    video_output = path_video / name
 
-    cmd = f" {ffmpeg_path} " \
-          f" -i {video_m4s} -i {audio_m4s} " \
+    cmd = f" {path_ffmpeg.as_posix()} " \
+          f" -i {video.video} -i {video.audio} " \
           f" -c:v copy " \
-          f" -strict experimental " \
           f" \"{video_output}.mp4\" "
-    logging.info('-' * 66)
-    logging.info(cmd)
-    logging.info('-' * 66)
+    cmd = cmd.strip()
+    print()
+    print(cmd)
+    print()
     os.system(cmd)
     # subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 
-def clear():
-    check = input("\n\n\n #删除下载文件夹# \n\n\n \t\t\t")
-    if check in ['1', 'y', '\\']:
-        shutil.rmtree(base_folder)
+def clear_download():
+    print()
+    print(f"  [red]# {path_download}[/red]")
+    check = Prompt.ask("  [red]# 删除下载文件夹 [/red]")
+    if check == '\\':
+        print("")
+        try:
+            shutil.rmtree(path_download)
+        except Exception as e:
+            pass
     print()
     print()
 
@@ -228,10 +222,11 @@ def test():
 
 
 def setup():
-    logging_config()
+    print_config()
 
-    if not os.path.exists(base_folder):
-        logging.error(f"不存在路径 {base_folder}")
+    if not path_download.exists():
+        print()
+        print(f"  不存在路径 {path_download}")
         return
 
     collections = init_collections()
@@ -240,7 +235,7 @@ def setup():
         for video in collection.videos:
             join_video(video)
 
-    clear()
+    # clear_download()
 
 
 if __name__ == '__main__':
